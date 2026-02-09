e41b2294258a3c3a0646cae877c0e7bbb27b52df
GhostGN95 <wcxie1234@gmail.com>
2026-02-09 23:30:44 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,20 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['sprint-be-project.s3.ap-northeast-2.amazonaws.com'],
+    domains: [
+      'localhost', 
+      '3.39.195.73', // 내 EC2 IP (혹시 몰라 추가)
+      's3.ap-northeast-2.amazonaws.com', // AWS S3 기본 도메인
+      // 만약 버킷 이름이 포함된 긴 도메인을 쓴다면 아래처럼 추가
+      'codeit-final-project-security.s3.ap-northeast-2.amazonaws.com', 
+    ],
+    // 또는 최신 Next.js 버전이라면 remotePatterns 사용 권장 (더 안전함)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com', // 아마존 모든 주소 허용
+      },
+    ],
   },
   webpack(config: Configuration) {
     config.module?.rules?.push({
